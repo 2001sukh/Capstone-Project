e465f8d1b6ff80330f0916f56ba65ef55b32783c
2001sukh <113390606+2001sukh@users.noreply.github.com>
2024-02-25 20:23:06 -0800

    Implement toggleMobileMenu() function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function toggleMobileMenu() {
     var menu = document.getElementById('nav-menu');
     menu.classList.toggle('active');
+
+    var toggle = document.getElementById('mobile-menu-toggle');
+    toggle.classList.toggle('active');
+
 }
 
 document.getElementById('mobile-menu-toggle').addEventListener('click', toggleMobileMenu);
