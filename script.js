ed826e80cbc10dbb9dbcfb4a06e574b837db0f6c
2001sukh <113390606+2001sukh@users.noreply.github.com>
2024-02-25 20:25:42 -0800

    Update mobile menu toggle button styling

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function toggleMobileMenu() {
     var bar1 = toggle.children[0];
     var bar2 = toggle.children[1];
     var bar3 = toggle.children[2];
-}
 
+    if (toggle.classList.contains('active')) {
+        bar1.style.transform = 'rotate(-45deg) translate(-5px, 6px)';
+        bar2.style.opacity = '0';
+        bar3.style.transform = 'rotate(45deg) translate(-5px, -6px)';
+    } 
+}   
 document.getElementById('mobile-menu-toggle').addEventListener('click', toggleMobileMenu);
